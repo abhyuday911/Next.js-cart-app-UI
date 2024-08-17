4a09c5ea62317b1addca29af18123cc6384273eb
Abhyuday <abhyuday911@gmail.com>
2024-08-17 22:40:48 +0530

    feat: Add remixicon npm dependency and import required CSS file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import 'remixicon/fonts/remixicon.css';
 import {CartContext, CartProvider} from "@/contexts/CartContext";
 
 const inter = Inter({ subsets: ["latin"] });
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,7 @@
 
 import { CartContext } from "@/contexts/CartContext";
 import { useTheme } from "@/hooks/useTheme";
+import Image from "next/image";
 import Link from "next/link";
 import React, { useContext } from "react";
 
@@ -10,11 +11,26 @@ const Nav = () => {
   const [theme, toggleTheme] = useTheme();
   return (
     <div className="fixed top-0 right-0 z-10 px-4 py-3 font-medium text-md gilroy bg-zinc-950 w-full">
-      <div className="flex items-center  h-full gap-8 text-zinc-300 px-2 lg:px-8">
-        <Link href="/" className="hover:scale-105">
-          Home
+      <div className="flex items-center  h-full gap-4 md:gap-8 text-zinc-300 px-2 lg:px-8">
+        <Link href="/" className="hover:scale-105 items-center gap-2 hidden md:flex">
+          <i className="ri-home-line text-xl"></i>
+          <h5 className="text-xl font-semibold">Shopping Kart</h5>
         </Link>
-        <Link href="/cart" className="hover:scale-105 ml-auto">
+        <div className="w-full md:w-[40%] ml-auto">
+          <div className="flex items-center w-full h-10 rounded-full focus-within:shadow-lg bg-neutral-200 overflow-hidden">
+            <div className="grid place-items-center h-full w-12 text-gray-600">
+              <i className="ri-search-2-line ri-lg"></i>
+            </div>
+            <input
+              className="peer h-full w-full outline-none text-base font-medium text-gray-700 pr-2 bg-neutral-200"
+              type="text"
+              id="search"
+              placeholder="Search something.."
+            />
+          </div>
+        </div>
+        <Link href="/" className="hover:scale-105 ml-auto"></Link>
+        <Link href="/cart" className="hover:scale-105">
           <div className="relative py-2">
             {Object.keys(cart).length > 0 && (
               <div className="-top-1 absolute left-3">
@@ -45,25 +61,22 @@ const Nav = () => {
           className="p-2 rounded-md text-gray-300 bg-zinc-900 bg-transparent dark:bg-gray-700 focus:outline-none "
         >
           {theme === "dark" ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-6 w-6"
-            >
-              <path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"></path>
-            </svg>
+            <i className="ri-sun-line ri-xl"></i>
           ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-6 w-6"
-            >
-              <path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"></path>
-            </svg>
+            <i className="ri-moon-line ri-xl"></i>
           )}
         </button>
+        <div className="h-10 w-10 border border-zinc-600 rounded-full bg-zinc-200 relative">
+          <Image
+            src="/placeholder.png"
+            alt="profile pic"
+            className="h-12 w-12 rounded-full"
+            fill={true}
+            priority={false}
+            style={{ objectFit: "contain" }}
+            sizes="(max-width: 768px) 100vw, 33vw"
+          ></Image>
+        </div>
       </div>
     </div>
   );
